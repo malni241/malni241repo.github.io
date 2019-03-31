f678d43569a85c6fb87e8f5996b6c1da9c06998e
Chri-Home <christophklinar@gmail.com>
2019-03-31 22:05:12 +0200

    + toolbar

---
+++ image-gallery/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav>
+        <v-toolbar app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Image</span>
+                <span>Gallery</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer app v-model="drawer"  class="indigo">
+            <p>test</p>
+        </v-navigation-drawer>
+
+    </nav>
+</template>
+
+
+<script>
+    export default {
+        data(){
+            return{
+                drawer: false
+            }
+        }
+    }
+</script>
---
+++ image-gallery/src/views/Playground.vue
@@ -0,0 +1,20 @@
+<template>
+ <div class="Playground">
+   <h1>Playground</h1>
+     <p class="red white--text">lorem </p>
+     <p class="pink lighten-4 red--text">lorem  dfasdf sdf asdf sadf sadf sdaf asdf </p>
+     <h1 class="display-4">Massive Display</h1>
+     <h4 class="display-1">Smaller Display</h4>
+     <p class="headline">this is a headline</p>
+     <p class="subheading font-weight-bold">this is a subheading.</p>
+     <p class="caption">this is a caption</p>
+ </div>
+</template>
+
+<script>
+
+
+  export default {
+
+  }
+</script>
