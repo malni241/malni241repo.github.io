4ac6d9ddeaf33a69938d1849d2f1a0767e7c1171
Unknown <christophklinar@gmail.com>
2019-04-09 16:41:47 +0200

    added db connection to users list

--- image-gallery/src/views/Users.vue
+++ image-gallery/src/views/Users.vue
@@ -4,15 +4,15 @@
 
     <v-container class="my-5">
       <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+        <v-flex xs12 sm6 md4 lg3 v-for="user in users" :key="user.email">
           <v-card flat class="text-xs-center ma-3">
             <v-responsive class="pt-4">
-                <vue-initials-img :name="person.name"/>
+                <vue-initials-img :name="user.name"/>
             </v-responsive>
             <v-car-text>
-              <div class="subheading">{{person.name}}</div>
-              <div class="grey--text">{{person.role}}</div>
-              <div class="grey--text">Registrationdate: {{person.registrationDate}}</div>
+              <div class="subheading">{{user.name}}</div>
+              <div class="grey--text">{{user.title}}</div>
+              <div class="grey--text">E-Mail: {{user.email}}</div>
             </v-car-text>
             <v-card-actions>
               <v-btn-flat color="grey">
@@ -28,9 +28,37 @@
 </template>
 
 <script>
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/storage'
+
+    let users = [];
+
+    const db_users = firebase.firestore().collection('users');
+    db_users.onSnapshot(function(querySnapshot) {
+
+      users = [];
+
+      querySnapshot.forEach(function(doc) {
+              
+        let u = {
+          name: doc.data().name,
+          title: doc.data().title,
+          email: doc.data().email,
+        };
+
+        users.push(u);
+      });
+
+
+    });
+
+
+
 export default {
   data() {
     return {
+      users: users,
       team: [
         { name: "Christoph", role: "Web dev", registrationDate: "02.03.2018" },
         { name: "Paul", role: "Photographer", registrationDate: "02.03.2018" },
