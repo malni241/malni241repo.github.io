6d53dffa41409547d2747caef4887faf7df6abdd
Unknown <christophklinar@gmail.com>
2019-04-09 13:55:39 +0200

    bugfix users

--- image-gallery/src/main.js
+++ image-gallery/src/main.js
@@ -3,12 +3,17 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import VueInitialsImg from 'vue-initials-img';
+import Vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+
 Vue.use(VueInitialsImg);  // image avatar for users
+
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(Vuetify);
